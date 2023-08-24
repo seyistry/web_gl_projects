98bd3fe9385a524cc49650e1ddd4c9eade1ff305
Oluwaseyi Egunjobi <seyistry@gmail.com>
2023-08-24 12:54:56 +0100

    test and update script

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -1,39 +1,105 @@
-import * as THREE from 'three'
+import * as THREE from 'three';
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector('canvas.webgl');
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+
+const group = new THREE.Group();
+scene.add(group);
+group.position.y = -0.6;
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+
+cube2.position.x = -1.5;
+
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+
+cube3.position.x = 1.5;
+
+group.add(cube3);
+
+// group.position.y = Math.PI ^ 0.25
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const mesh = new THREE.Mesh(geometry, material);
+// scene.add(mesh);
+
+// Position
+
+// mesh.position.set(0.7, 1, 1);
+
+// Scale
+// mesh.scale.set(2, 0.5, 0.5);
+
+// Rotation
+
+// mesh.rotation.x = Math.PI * 0.25;
+// mesh.rotation.y = Math.PI * 0.25;
+// mesh.rotation.z = Math.PI * 0.25;
+
+// console.log(mesh.position.distanceTo(camera.position));
+// mesh.position.normalize()
+
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
 
 /**
  * Sizes
  */
 const sizes = {
-    width: 800,
-    height: 600
-}
+  width: 800,
+  height: 600,
+};
 
 /**
  * Camera
  */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+// camera.position.z = 3;
+camera.position.set(0, 1, 5);
+scene.add(camera);
+
+// camera.lookAt(mesh.position)
+// console.log(mesh.position.distanceTo(camera.position));
 
 /**
  * Renderer
  */
+
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+
+// function animate() {
+//   requestAnimationFrame(animate);
+
+//   group.rotation.x += 0.01;
+//   group.rotation.y += 0.01;
+
+//   renderer.render(scene, camera);
+// }
+
+// animate();
