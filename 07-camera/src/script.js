61f49634866e85b1314e07dc8d697ec6729c6a33
Oluwaseyi Egunjobi <seyistry@gmail.com>
2023-08-29 23:33:18 +0100

    add 07 camera ex

---
+++ 07-camera/src/script.js
@@ -0,0 +1,95 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+//cursor
+// const cursor = {
+//   x: 0,
+//   y: 0,
+// };
+
+// window.addEventListener('mousemove', (event) => {
+//   cursor.x = event.clientX / sizes.width - 0.5;
+//   cursor.y = -(event.clientY / sizes.height - 0.5);
+//   console.log(cursor.x);
+// });
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const mesh = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+scene.add(mesh);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+
+// const aspectRatio = sizes.width / sizes.height;
+
+// const camera = new THREE.OrthographicCamera(
+//   -aspectRatio,
+//   aspectRatio,
+//   1,
+//   -1,
+//   1,
+//   100
+// );
+// camera.position.x = 3;
+// camera.position.y = 3;
+camera.position.z = 3;
+// console.log(camera.position.length());
+camera.lookAt(mesh.position);
+scene.add(camera);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+
+// Animate
+// const clock = new THREE.Clock();
+
+const controls = new OrbitControls(camera, canvas);
+// camera.position.set( 0, 20, 100 );
+controls.enableDamping = true;
+// controls.update();
+
+const tick = () => {
+  //   const elapsedTime = clock.getElapsedTime();
+
+  // Update objects
+  //   mesh.rotation.y = elapsedTime;
+
+  //update camera
+  //   camera.position.x = Math.cos(cursor.x * Math.PI * 2) * Math.PI;
+  //   camera.position.y = cursor.y * 10;
+  //   camera.position.z = Math.sin(cursor.x * Math.PI * 2) * Math.PI;
+  //   camera.lookAt(mesh.position);
+  //   camera.position.z = 3;
+  // Render
+  controls.update();
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
